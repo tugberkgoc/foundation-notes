6439431765e985e081acdd876425ec3535bc1093
Mark Tyers <aa7401@coventry.ac.uk>
2018-01-13 20:56:23 +0000

    added simple express example

---
+++ exercises/01_setup/index.js
@@ -0,0 +1,14 @@
+
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
+app.listen(port, () => {
+	console.log(`app listening on port ${port}`)
+})
