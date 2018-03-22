b4e7f56d08f675e8d536aa21b00d8c59ff514235
Mark Tyers <marktyers@marks-macbook.home>
2018-03-22 07:22:17 +0000

    fixed file name issue

--- exercises/12_spa/localstorage/public/js/localstorage.js
+++ exercises/12_spa/localstorage/public/js/localstorage.js
@@ -3,9 +3,10 @@
 
 console.log('page loaded')
 
-document.querySelector('button').onclick = () => {
+const btn = window.document.querySelector('button')
+btn.addEventListener('click', () => {
 	const newItem = document.querySelector('input').value
 	const items = localStorage.items ? JSON.parse(localStorage.items) : []
 	items.push(newItem)
 	localStorage.items = JSON.stringify(items)
-}
+})
