ae0e13b00950adaf95c1ba7b74b2e53afff155e6
Mark Tyers <aa7401@coventry.ac.uk>
2018-07-02 15:17:57 +0100

    completed simple login

--- exercises/13_website/authentication/index.js
+++ exercises/13_website/authentication/index.js
@@ -18,21 +18,22 @@ const port = 8080
 const sqlite3 = require('sqlite3').verbose()
 
 const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 const session = require('express-session')
 const FileStore = require('session-file-store')(session)
 app.use(session({
-    name: 'server-session-cookie-id',
-    secret: 'my express secret',
-    saveUninitialized: true,
-    resave: true,
-    store: new FileStore()
+	name: 'server-session-cookie-id',
+	secret: 'my express secret',
+	saveUninitialized: true,
+	resave: true,
+	store: new FileStore()
 }))
 
-app.use(function printSession(req, res, next) {
-    console.log('req.session', req.session)
-    return next()
-  })
+app.use( (req, res, next) => {
+	console.log('req.session', req.session)
+	return next()
+})
 
 const db = new sqlite3.Database('./auth.db', err => {
 	if (err) return console.error(err.message)
@@ -40,27 +41,64 @@ const db = new sqlite3.Database('./auth.db', err => {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/html/index.html`)
+	if(!req.session.authenticated) res.redirect('/login')
+	res.sendFile(`${__dirname}/html/index.html`)
 })
 
 app.get('/register', (req, res) => {
-    res.sendFile(`${__dirname}/html/register.html`)
+	res.sendFile(`${__dirname}/html/register.html`)
 })
 
 app.post('/register', (req, res) => {
-    console.log(req.body)
-    bcrypt.hash(req.body.pass, 10, (err, hash) => {
-        req.body.pass = hash
-        console.log(req.body)
-        const sql = `INSERT INTO users(name, username, email, password) 
-                    VALUES("${req.body.name}", "${req.body.user}", "${req.body.email}", "${req.body.pass}")`
-        console.log(sql)
-        db.run(sql, err => {
-            if(err) console.error(err.message)
-            console.log('new account added')
-            //res.redirect('/')
-        })
-    })
+	console.log(req.body)
+	bcrypt.hash(req.body.pass, saltRounds, (err, hash) => {
+		if(err) console.error(err.message)
+		req.body.pass = hash
+		console.log(req.body)
+		const sql = `INSERT INTO users(name, username, email, password) 
+					VALUES("${req.body.name}", "${req.body.user}", "${req.body.email}", "${req.body.pass}")`
+		console.log(sql)
+		db.run(sql, err => {
+			if(err) console.error(err.message)
+			console.log('new account added')
+			res.redirect('/')
+		})
+	})
+})
+
+app.get('/login', (req, res) => {
+	res.sendFile(`${__dirname}/html/login.html`)
+})
+
+app.post('/login', (req, res) => {
+	if(req.body.pass === "") req.body.pass = ' '
+	const sql = `SELECT count(id) AS count FROM users WHERE username="${req.body.user}";`
+	db.get(sql, (err, data) => {
+		if(err) console.error(err.message)
+		if(!data.count) {
+			console.log('invalid username')
+			return res.redirect('/login?message=invalid%20username')
+		}
+		const sql2 = `SELECT password FROM users WHERE username = "${req.body.user}";`
+		db.get(sql2, (err, data) => {
+			if(err) console.error(err.message)
+			console.log(req.body.pass)
+			bcrypt.compare(req.body.pass, data.password, (err, data) => {
+				console.log('bcrypt')
+				console.log(data)
+				if(err) console.error(err.message)
+				if(data === true) {
+					req.session.authenticated = true
+				}
+				return res.redirect('/')
+			})
+		})
+	})
+})
+
+app.get('/logout', (req, res) => {
+	delete req.session.authenticated
+	return res.redirect('/')
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
