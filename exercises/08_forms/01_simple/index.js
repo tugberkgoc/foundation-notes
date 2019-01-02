56cbd014bc4e477f3416a47a45f91574a677aaf4
Mark Tyers <aa7401@coventry.ac.uk>
2019-01-02 13:31:10 +0000

    updated forms to koa

--- exercises/08_forms/01_simple/index.js
+++ exercises/08_forms/01_simple/index.js
@@ -1,57 +1,46 @@
+#!/usr/bin/env node
 
-'use strict'
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
-
+const Koa = require('koa')
+const Router = require('koa-router')
+const app = new Koa()
+const router = new Router()
+const views = require('koa-views')
+app.use(require('koa-static')('public'))
+const bodyParser = require('koa-bodyparser')
+app.use(bodyParser())
 const port = 8080
 
-app.get('/postform', (req, res) => {
-	res.sendFile(`${__dirname}/html/post.html`)
+app.use(views(`${__dirname}/views`, { extension: 'html' }, {map: { handlebars: 'handlebars' }}))
+
+router.get('/postform', async ctx => await ctx.render('post'))
+router.get('/getform', async ctx => await ctx.render('get'))
+router.get('/lists', async ctx => await ctx.render('lists'))
+router.get('/semantic', async ctx => await ctx.render('form-skel'))
+
+router.get('/getform/process', async ctx => {
+	console.log(ctx.query)
+	let data = '<html><body><h1>Retrieving Data in Querystring</h1><table>'
+	for(const param in ctx.query) {
+		console.log(param)
+		data += `<tr><td>${param}</td><td>${ctx.query[param]}</td></tr>`
+	}
+	data += '</table></body></html>'
+	ctx.body = data
 })
 
-app.get('/getform', (req, res) => {
-	res.sendFile(`${__dirname}/html/get.html`)
-})
-
-app.get ('/lists', (req, res) => {
-	res.sendFile(`${__dirname}/html/lists.html`)
-})
-
-app.get ('/semantic', (req, res) => {
-	res.sendFile(`${__dirname}/html/form-skel.html`)
-})
-
-// this route processes data in the querystring
-app.get('/', (req, res) => {
-	res.write('<html><body><h1>Retrieving Data in Querystring</h1><table>')
-	for (const param in req.query) {
-		res.write(`<tr><td>${param}</td><td>${req.query[param]}</td></tr>`)
- 	}
-	res.write('</table></body></html>')
-})
-
-// this route processes data in the request body
-app.post('/', (req, res) => {
-
-	console.log(req.body)
-
-	const data = req.body
-	res.write('<html><body><h2>Retrieving Data in Body</h2><table>')
-	for (const key in data) {
-		if (data.hasOwnProperty(key)) {
+router.post('/postform/process', async ctx => {
+	console.log(ctx.request.body)
+	let data = '<html><body><h2>Retrieving Data in Body</h2><table>'
+	for (const key in ctx.request.body) {
+		if (ctx.request.body.hasOwnProperty(key)) {
 			console.log(key)
-			console.log(data[key])
-			res.write(`<tr><td>${key}</td><td>${data[key]}</td></tr>`)
+			console.log(ctx.request.body[key])
+			data += `<tr><td>${key}</td><td>${ctx.request.body[key]}</td></tr>`
 		}
 	}
-	res.write('</table></body></html>')
-	res.end()
+	data += '</table></body></html>'
+	ctx.body = data
 })
 
-app.listen(port, () => {
-	console.log(`app listening on port ${port}`)
-})
+app.use(router.routes())
+module.exports = app.listen(port, () => console.log(`listening on port ${port}`))
