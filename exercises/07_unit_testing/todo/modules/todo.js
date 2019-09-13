433c90515974e5642ac6d3a0de019043c143d0b5
Mark Tyers <aa7401@coventry.ac.uk>
2019-09-13 09:42:58 +0100

    added base code for tests

---
+++ exercises/07_unit_testing/todo/modules/todo.js
@@ -0,0 +1,23 @@
+
+'use strict'
+
+let data = []
+
+module.exports.clear = () => {
+	data = []
+}
+
+module.exports.add = (item, qty) => {
+	data.push({item: item, qty: qty})
+}
+
+module.exports.getAll = () => {
+	if(data.length === 0) throw new Error('empty list')
+	for(const key in data) data[key].key = key
+	return data
+}
+
+module.exports.delete = key => {
+	console.log(`delete key ${key}`)
+	return
+}
